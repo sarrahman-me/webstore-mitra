b99b942b5d3c310a040931f6846d4a1212d0fc6c
sarrahman-me <sarrahman.me@gmail.com>
2023-09-13 08:25:14 +0700

    :hammer: menambahkan request data membership

--- app/api/webstore/route.ts
+++ app/api/webstore/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_HOST}/webstore/domain/berhasil.netlify.app`,
+      `${process.env.NEXT_PUBLIC_HOST}/webstore/domain/gaung.netlify.app`,
       {
         headers: {
           "Content-Type": "application/json",
@@ -14,12 +14,25 @@ export async function GET(request: Request) {
     );
     const data = await response.json();
 
+    const membershipResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST}/membership/member/${data.data.id_membership}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+        credentials: "include",
+      }
+    );
+    const membership = await membershipResponse.json();
+
     const hostname = `hostname: ${request.headers.get("host")}`;
 
     return NextResponse.json({
       status: 200,
       success: true,
       data: data.data,
+      membership: membership.data,
       hostname,
       requestUrl: request.url,
       requestHost: request.headers.get("host"),
