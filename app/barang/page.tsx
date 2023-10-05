8448449e1aaf07b6d181cfbd40a065420550562d
sarrahman-me <sarrahman.me@gmail.com>
2023-10-05 14:49:11 +0700

    :bug: perubahan pada halaman catalog barang

--- app/barang/page.tsx
+++ app/barang/page.tsx
@@ -5,36 +5,33 @@ import { AppBar } from "@/src/layouts";
 import { GetDataApi } from "@/src/utils";
 import { useEffect, useState } from "react";
 
-const Barang = () => {
-  const [data, setdata] = useState({} as any);
+const Home = () => {
   const [barangPromo, setbarangPromo] = useState([] as any);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await GetDataApi("/api/webstore");
       const responseBarangPromo = await GetDataApi(
         `${process.env.NEXT_PUBLIC_HOST}/products/barang?promo=true`
       );
       setbarangPromo(responseBarangPromo.data);
-      setdata(response.data);
     }
     fetchData();
   }, []);
 
   return (
     <div>
-      <AppBar data={data} />
+      <AppBar nama_webstore={"test"} />
       <SearchBar />
       <SwiperProduct
-        persentaseHarga={data.profit_percentage}
-        url="/dashboard/barang/promo"
+        persentaseHarga={5}
+        url="/barang/promo"
         title="Promo"
         products={barangPromo}
       />
       <p className="underline font-semibold m-2">{"Semua Barang"}</p>
-      <CatalogProducts persentaseHarga={data.profit_percentage} />
+      <CatalogProducts persentaseHarga={5} />
     </div>
   );
 };
 
-export default Barang;
+export default Home;
