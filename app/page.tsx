6c9113a7322cd7cb4fea7c066ee442f4e3bffb58
sarrahman-me <sarrahman.me@gmail.com>
2023-11-26 20:39:45 +0800

    :hammer: menambahkan tombol whatsapp

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { CatalogProducts, SwiperProduct } from "@/src/components/oraganisms";
 import { AppBar, MotifList, PilihanPencarian } from "@/src/layouts";
 import { GetDataApi } from "@/src/utils";
 import { useEffect, useState } from "react";
+import { RiWhatsappFill } from "react-icons/ri";
 import { useSelector } from "react-redux";
 
 const Home = () => {
@@ -20,6 +21,17 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const whatsappNumber = "+6282157758174";
+
+  const handlePesan = () => {
+    const message = `Halo Admin`;
+    const whatsappLink = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(
+      message
+    )}`;
+
+    window.open(whatsappLink, "_blank");
+  };
+
   return (
     <div>
       <AppBar />
@@ -35,6 +47,15 @@ const Home = () => {
       <PilihanPencarian />
       <p className="underline font-semibold m-2">Semua Barang</p>
       <CatalogProducts />
+      <div className="fixed right-0 bottom-0 m-2">
+        <div
+          onClick={handlePesan}
+          className="bg-blue-50 dark:bg-slate-800 space-x-2 flex items-center rounded-full p-2 px-4"
+        >
+          <RiWhatsappFill className="text-green-500 text-3xl" />
+          <p>Chat</p>
+        </div>
+      </div>
     </div>
   );
 };
